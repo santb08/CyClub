a89f621aa382769e07f2eb91d6fa71ed64317bbc
fxfxrxfx <santi-c-b@outlook.com>
2019-08-29 01:56:55 -0500

    Graphing routes: Done

--- lib/Map.dart
+++ lib/Map.dart
@@ -1,8 +1,11 @@
+import 'package:cyclub/helpers/polylines.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
 import 'package:location/location.dart';
+import 'package:cyclub/helpers/api.dart';
+import 'package:http/http.dart' as http;
 
 class Map extends StatefulWidget {
   @override
@@ -15,6 +18,24 @@ class _Map extends State<Map>{
 
   //Polylines for trace the routes
   Set<Polyline> _polylines = {};
+  
+  bool _loadingRoutes;
+
+  //Fake data, in case location get some err(?) **Not really needed**
+  static final CameraPosition _kInitial = CameraPosition(
+    tilt: 45,
+    target: LatLng(6.229548, -75.5705),//37.42796133580664
+    zoom: 14.4746,
+  );
+
+  _loadPolylines() async {
+    Set<Polyline> pl = await getRoutesPolylines();
+    setState(() {
+      _polylines.addAll(pl);
+      _loadingRoutes = false;
+      print(_polylines);
+    });
+  }
 
   /**
    * initState will initialize the position of the app at user's
@@ -24,31 +45,20 @@ class _Map extends State<Map>{
   initState(){
     super.initState();
     _goToMyPos();
-    //TODO: Get routes and put them in the state
-    _polylines.add(Polyline(
-      polylineId: PolylineId('Ejemplo'),
-      visible: true,
-      points: [LatLng(6.23116127, -75.59998208), LatLng(6.23182977, -75.57790234)],
-      width: 3,
-      color: Colors.blue,
-    ));
-    print("EO");
-  }
+    _loadingRoutes = true;
+    _loadPolylines();
 
-  //Fake data, in case location get some err(?) **Not really needed**
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    tilt: 45,
-    target: LatLng(6.229548, -75.5705),//37.42796133580664
-    zoom: 14.4746,
-  );
+  }
 
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: GoogleMap(
+    return Scaffold(
+      body: _loadingRoutes ? Center(
+                child: CircularProgressIndicator(),
+              ) :  GoogleMap(
         mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
+        initialCameraPosition: _kInitial,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
