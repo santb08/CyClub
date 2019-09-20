58f7f47376838ea8d835901a83ce7e461dfb7e74
fxfxrxfx <santi-c-b@outlook.com>
2019-09-19 21:08:34 -0500

    Avatar implemented for Profile

--- lib/Map.dart
+++ lib/Map.dart
@@ -1,5 +1,6 @@
 //ICONS https://www.flaticon.com/authors/freepik
 
+import 'package:cyclub/Profile.dart';
 import 'package:cyclub/helpers/polylines.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -83,21 +84,44 @@ class _Map extends State<Map>{
             markers: Set<Marker>.from([myLocation]),
             polylines: _polylines
           ),
-          Stack(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(bottom: 0),
-                width: 300,
-                height: 300,
-                decoration: BoxDecoration(
-                    color: Colors.blueAccent,
-                    borderRadius: BorderRadius.all(Radius.circular(150)),
-                    boxShadow: [
-                      BoxShadow(blurRadius: 15, color: Colors.black)
-                    ]
+          Positioned(
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  bottom: -80,
+                  left: -60,
+                  child: Container(
+                    width: 225,
+                    height: 225,
+                    decoration: BoxDecoration(
+                      color: Colors.limeAccent,
+                      borderRadius: BorderRadius.all(Radius.circular(155)),
+                      boxShadow: [
+                        BoxShadow(blurRadius: 25, color: Colors.black)
+                      ]
+                    ),
                   ),
-              ),
-            ],
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: -10,
+                  child: GestureDetector(
+                    onTap: () => Navigator.push(context, _createRoute()),
+                    child: Container(
+                      width: 167,
+                      height: 180,
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        image: DecorationImage(
+                          image: AssetImage("assets/Panita.png"),
+                          fit: BoxFit.cover
+                        )
+                      ),
+                    ),
+                  ),
+                ),
+              ]
+            ),
           ),
         ],
       )
@@ -105,6 +129,23 @@ class _Map extends State<Map>{
     );
   }
 
+  Route _createRoute() {
+        return PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) => Profile(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            var begin = Offset(0.0, 1.0);
+            var end = Offset.zero;
+            var curve = Curves.ease;
+
+            var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+            return SlideTransition(
+              position: animation.drive(tween),
+              child: child,
+            );
+          },
+        );
+      }
   /**
    * According to @target it'll make the map move right here
    */
