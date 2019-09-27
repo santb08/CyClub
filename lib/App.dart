0c859615be6280c86213a980639299b631c6eee7
JQartas <jcuartas0611@gmail.com>
2019-09-26 23:00:28 -0500

    Implementaciones en el sidemenu y en el login

--- lib/App.dart
+++ lib/App.dart
@@ -24,22 +24,98 @@ class SideBarMenu extends StatelessWidget {
 
   Widget build(context) {
     return Drawer(
-      child: ListView( 
+      child: ListView(
         children: <Widget>[
           DrawerHeader(
-            child: Text("Menu"),
-            decoration: BoxDecoration(color: Colors.blue),
+            child: Text('Menu', style: TextStyle(color: Colors.white,fontSize: 25),),
+            curve: Curves.easeInCirc,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/drawer_header_background.jpg')
+              )
+            )
+          ),
+          MaterialButton(
+            onPressed: () => print('perfil'),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(Icons.person_pin,color: Colors.lime,size: 40,),
+                Padding(child: Text('Mi perfil', style: TextStyle(color: Colors.black)), padding: EdgeInsets.symmetric(vertical: 13,horizontal: 10),)
+              ],
+            ),
+          ),
+          MaterialButton(
+            onPressed: () => print('amigos'),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(Icons.people_outline,color: Colors.lime,size: 40,),
+                Padding(child: Text('Amigos', style: TextStyle(color: Colors.black)), padding: EdgeInsets.symmetric(vertical: 13,horizontal: 10),)
+              ],
+            ),
+          ),
+          MaterialButton(
+            onPressed: () => print('config'),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(Icons.settings,color: Colors.lime,size: 40,),
+                Padding(child: Text('Configuración', style: TextStyle(color: Colors.black)), padding: EdgeInsets.symmetric(vertical: 13,horizontal: 10),)
+              ],
+            ),
+          ),
+          MaterialButton(
+            onPressed: () => print('lugares'),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(Icons.place,color: Colors.lime,size: 40,),
+                Padding(child: Text('Lugares', style: TextStyle(color: Colors.black)), padding: EdgeInsets.symmetric(vertical: 13,horizontal: 10),)
+              ],
+            ),
+          ),
+          MaterialButton(
+            onPressed: () => print('retos'),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(Icons.score,color: Colors.lime,size: 40,),
+                Padding(child: Text('Retos', style: TextStyle(color: Colors.black)), padding: EdgeInsets.symmetric(vertical: 13,horizontal: 10),)
+              ],
+            ),
+          ),
+          MaterialButton(
+            onPressed: () => print('compartir'),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(Icons.share,color: Colors.lime,size: 40,),
+                Padding(child: Text('Compartir', style: TextStyle(color: Colors.black)), padding: EdgeInsets.symmetric(vertical: 13,horizontal: 10),)
+              ],
+            ),
+          ),
+          MaterialButton(
+            color: Colors.redAccent,
+            onPressed: () => print('llamada'),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Icon(Icons.call,size: 40,),
+                Padding(padding: EdgeInsets.symmetric(vertical: 12, horizontal: 10),child:Text('Llamada de emergencia'),)
+              ],
+            )
           ),
           ListTile(
-            title: Text("Mi perfil"),
-            onTap: () => Navigator.push(context, _createRoute()),
+            title: Text("Conocenos"),
+            subtitle: Text('Acerca de nosotros...'),
+            onTap: () => print('about'),
           ),
-          ListTile(
-            title: Text("Otra opción"),
-            onTap: () => Navigator.push(context, _createRoute()),
-          ),
-        ]
-      ), // appBar: SideMenu(),
+        ],        
+      ),
     );
   }
 }
